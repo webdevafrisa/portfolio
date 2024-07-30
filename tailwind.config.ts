9319a128c39f283bcf6d8db8c819cc03a99f6cd0
webdevAfrisa <webdevafrisa@gmail.com>
2024-07-30 19:43:13 +0300

    Initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,23 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+
+       animation: {
+     'logo-cloud': 'logo-cloud 30s linear infinite', // Adjust duration and timing as needed for your design.
+   },
+
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      keyframes: {
+             'logo-cloud': {
+               from: { transform: 'translateX(0)' },
+               to: { transform: 'translateX(calc(-100% - 4rem))' },
+             },
+           }
     },
   },
   plugins: [],
